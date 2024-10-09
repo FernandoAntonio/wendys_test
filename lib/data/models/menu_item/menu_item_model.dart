73bd31fcf2c9dac6a9a2e75e569b17892ccc5fbb
Fernando Antonio <fernandojr.semrhfj@hotmail.com>
2024-10-09 13:29:13 -0300

    feat(mobile-app): add menu item model

---
+++ lib/data/models/menu_item/menu_item_model.dart
@@ -0,0 +1,31 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'menu_item_model.freezed.dart';
+part 'menu_item_model.g.dart';
+
+@freezed
+class MenuItemModel with _$MenuItemModel {
+  const MenuItemModel._();
+
+  const factory MenuItemModel({
+    required int menuItemId,
+    required String name,
+    required String displayName,
+    required String description,
+    @Default('Not available') String calorieRange,
+    @Default('Not available') String priceRange,
+  }) = _MenuItemModel;
+
+  factory MenuItemModel.empty() {
+    return MenuItemModel(
+      menuItemId: 0,
+      name: '',
+      displayName: '',
+      description: '',
+      calorieRange: '',
+      priceRange: '',
+    );
+  }
+
+  factory MenuItemModel.fromJson(Map<String, dynamic> json) => _$MenuItemModelFromJson(json);
+}
--- lib/data/models/menu_lists/menu_lists_model.dart
+++ lib/data/models/menu_lists/menu_lists_model.dart
@@ -1,5 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:wendys_test/data/models/category/category_model.dart';
+import 'package:wendys_test/data/models/menu_item/menu_item_model.dart';
 
 part 'menu_lists_model.freezed.dart';
 part 'menu_lists_model.g.dart';
@@ -11,7 +12,7 @@ class MenuListsModel with _$MenuListsModel {
   const factory MenuListsModel({
     // ignore: invalid_annotation_target
     @JsonKey(name: 'subMenus') required List<CategoryModel> categories,
-    required List menuItems,
+    required List<MenuItemModel> menuItems,
   }) = _MenuListsModel;
 
   factory MenuListsModel.empty() {
