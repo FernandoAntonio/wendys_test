8d06e614275baf9d54a072a9496d126881d10f74
Fernando Antonio <fernandojr.semrhfj@hotmail.com>
2024-10-10 13:50:03 -0300

    feat(mobile-app): add tests for menu local datasource

---
+++ test/data/datasources/menu/menu_local_datasource_test.dart
@@ -0,0 +1,107 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:wendys_test/core/local_storage/entities/local_storage_keys.dart';
+import 'package:wendys_test/core/local_storage/local_storage.dart';
+import 'package:wendys_test/data/datasources/menu/menu_local_datasource.dart';
+import 'package:wendys_test/data/models/category/category_model.dart';
+import 'package:wendys_test/data/models/menu_item/menu_item_model.dart';
+
+import '../../../fixtures/categories/categories_model_list.dart';
+import '../../../fixtures/fixture_reader.dart';
+import '../../../fixtures/menu_items/menu_items_model_list.dart';
+import 'menu_local_datasource_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<ILocalStorage>()])
+void main() {
+  late ILocalStorage mockLocalStorage;
+  late MenuLocalDatasource localDatasource;
+
+  setUp(() {
+    mockLocalStorage = MockILocalStorage();
+    localDatasource = MenuLocalDatasource(localStorage: mockLocalStorage);
+  });
+
+  group('MenuLocalDatasource', () {
+    final categoriesString = fixture('categories/categories_list');
+    final menuItemsString = fixture('menu_items/menu_items_list');
+
+    group('getCachedCategories', () {
+      test('should return a list of CategoryModel when the call is successful', () async {
+        // Arrange
+        when(mockLocalStorage.read(LocalStorageKeys.categories)).thenAnswer((_) async => categoriesString);
+
+        // Act
+        final result = await localDatasource.getCachedCategories();
+
+        // Assert
+        expect(result, isA<List<CategoryModel>>());
+        verify(mockLocalStorage.read(LocalStorageKeys.categories)).called(1);
+      });
+
+      test('should return an empty list when the call is unsuccessful', () async {
+        // Arrange
+        when(mockLocalStorage.read(LocalStorageKeys.categories)).thenAnswer((_) async => null);
+
+        // Act
+        final result = await localDatasource.getCachedCategories();
+
+        // Assert
+        expect(result, []);
+        verify(mockLocalStorage.read(LocalStorageKeys.categories)).called(1);
+      });
+    });
+
+    group('getCachedMenuItems', () {
+      test('should return a list of MenuItemModel when the call is successful', () async {
+        // Arrange
+        when(mockLocalStorage.read(LocalStorageKeys.menuItems)).thenAnswer((_) async => menuItemsString);
+
+        // Act
+        final result = await localDatasource.getCachedMenuItems();
+
+        // Assert
+        expect(result, isA<List<MenuItemModel>>());
+        verify(mockLocalStorage.read(LocalStorageKeys.menuItems)).called(1);
+      });
+
+      test('should return an empty list when the call is unsuccessful', () async {
+        // Arrange
+        when(mockLocalStorage.read(LocalStorageKeys.menuItems)).thenAnswer((_) async => null);
+
+        // Act
+        final result = await localDatasource.getCachedMenuItems();
+
+        // Assert
+        expect(result, []);
+        verify(mockLocalStorage.read(LocalStorageKeys.menuItems)).called(1);
+      });
+    });
+
+    group('storeCategories', () {
+      test('should write the correct data in local storage when successful', () async {
+        // Act
+        await localDatasource.storeCategories(categoriesModelList);
+
+        // Assert
+        verify(mockLocalStorage.write(
+          LocalStorageKeys.categories,
+          categoriesModelString,
+        )).called(1);
+      });
+    });
+
+    group('storeMenuItems', () {
+      test('should write the correct data in local storage when successful', () async {
+        // Act
+        await localDatasource.storeMenuItems(menuItemsModelList);
+
+        // Assert
+        verify(mockLocalStorage.write(
+          LocalStorageKeys.menuItems,
+          menuItemsModelString,
+        )).called(1);
+      });
+    });
+  });
+}
---
+++ test/fixtures/menu_items/menu_items_model_list.dart
@@ -0,0 +1,52 @@
+import 'package:wendys_test/data/models/menu_item/menu_item_model.dart';
+
+final menuItemsModelString =
+    '[{"menuItemId":30000,"name":"Dave\'s Single®","displayName":"Dave\'s Single®","description":"A quarter-pound* of fresh beef, American cheese, crisp lettuce, tomato, pickle, ketchup, mayo, and onion on a toasted bun — just the way Dave intended.","calorieRange":"590 Cal","priceRange":"\$3.59"},{"menuItemId":30001,"name":"Dave\'s Double®","displayName":"Dave\'s Double®","description":"A half-pound* of fresh beef, American cheese, crisp lettuce, tomato, pickle, ketchup, mayo, and onion on a toasted bun. More meat means more to love.","calorieRange":"860 Cal","priceRange":"\$4.69"},{"menuItemId":30002,"name":"Dave\'s Triple®","displayName":"Dave\'s Triple®","description":"Three quarters of a pound* of fresh, never frozen beef, American cheese, crisp lettuce, tomato, pickle, ketchup, mayo, and onion on a toasted bun. Use both hands!","calorieRange":"1,160 Cal","priceRange":"\$5.59"},{"menuItemId":30003,"name":"Baconator®","displayName":"Baconator®","description":"A half-pound* of fresh beef, American cheese, 6 pieces of crispy Applewood smoked bacon, ketchup, and mayo. Carnivores rejoice!","calorieRange":"960 Cal","priceRange":"\$5.89"},{"menuItemId":30004,"name":"Son of Baconator®","displayName":"Son of Baconator®","description":"Two patties of fresh, never frozen beef, American cheese, plenty of Applewood smoked bacon, ketchup, and mayo. Like Baconator®, like Son.","calorieRange":"630 Cal","priceRange":"\$3.99"}]';
+
+final menuItemsModelList = [
+  MenuItemModel(
+    menuItemId: 30000,
+    displayName: 'Dave\'s Single®',
+    name: 'Dave\'s Single®',
+    description:
+        'A quarter-pound* of fresh beef, American cheese, crisp lettuce, tomato, pickle, ketchup, mayo, and onion on a toasted bun — just the way Dave intended.',
+    priceRange: '\$3.59',
+    calorieRange: '590 Cal',
+  ),
+  MenuItemModel(
+    menuItemId: 30001,
+    displayName: 'Dave\'s Double®',
+    name: 'Dave\'s Double®',
+    description:
+        'A half-pound* of fresh beef, American cheese, crisp lettuce, tomato, pickle, ketchup, mayo, and onion on a toasted bun. More meat means more to love.',
+    priceRange: '\$4.69',
+    calorieRange: '860 Cal',
+  ),
+  MenuItemModel(
+    menuItemId: 30002,
+    displayName: 'Dave\'s Triple®',
+    name: 'Dave\'s Triple®',
+    description:
+        'Three quarters of a pound* of fresh, never frozen beef, American cheese, crisp lettuce, tomato, pickle, ketchup, mayo, and onion on a toasted bun. Use both hands!',
+    priceRange: '\$5.59',
+    calorieRange: '1,160 Cal',
+  ),
+  MenuItemModel(
+    menuItemId: 30003,
+    displayName: 'Baconator®',
+    name: 'Baconator®',
+    description:
+        'A half-pound* of fresh beef, American cheese, 6 pieces of crispy Applewood smoked bacon, ketchup, and mayo. Carnivores rejoice!',
+    priceRange: '\$5.89',
+    calorieRange: '960 Cal',
+  ),
+  MenuItemModel(
+    menuItemId: 30004,
+    displayName: 'Son of Baconator®',
+    name: 'Son of Baconator®',
+    description:
+        'Two patties of fresh, never frozen beef, American cheese, plenty of Applewood smoked bacon, ketchup, and mayo. Like Baconator®, like Son.',
+    priceRange: '\$3.99',
+    calorieRange: '630 Cal',
+  ),
+];
