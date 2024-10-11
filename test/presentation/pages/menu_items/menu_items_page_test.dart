3d6c8b27444ddc013575ec27c607e8498d97a179
Fernando Antonio <fernandojr.semrhfj@hotmail.com>
2024-10-11 16:17:08 -0300

    feat(mobile-app): add testing to menu items page

--- test/presentation/pages/menu_items/menu_items_page_test.dart
+++ test/presentation/pages/menu_items/menu_items_page_test.dart
@@ -4,25 +4,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:wendys_test/dependency_injection.dart';
 import 'package:wendys_test/presentation/cubit/cart/cart_cubit.dart';
-import 'package:wendys_test/presentation/cubit/categories/categories_cubit.dart';
 import 'package:wendys_test/presentation/cubit/menu_items/menu_items_cubit.dart';
-import 'package:wendys_test/presentation/pages/categories/categories_page.dart';
 import 'package:wendys_test/presentation/pages/menu_items/menu_items_page.dart';
+import 'package:wendys_test/presentation/pages/selected_menu_item/selected_menu_item_page.dart';
 
-import '../../../fixtures/categories/categories_model_list_mock.dart';
 import '../../../fixtures/categories/category_model_hamburger.dart';
+import '../../../fixtures/menu_items/menu_items_model_hamburgers.dart';
+import '../../../fixtures/menu_items/menu_items_model_list_mock.dart';
 import '../../../mocks/mocks.mocks.dart';
 
 void main() {
-  late CategoriesCubit mockCategoriesCubit;
   late MenuItemsCubit mockMenuItemsCubit;
   late CartCubit mockCartCubit;
 
   setUp(() {
-    mockCategoriesCubit = MockCategoriesCubit();
     mockMenuItemsCubit = MockMenuItemsCubit();
     mockCartCubit = MockCartCubit();
-    getIt.registerSingleton<CategoriesCubit>(mockCategoriesCubit);
     getIt.registerSingleton<MenuItemsCubit>(mockMenuItemsCubit);
   });
 
@@ -33,41 +30,36 @@ void main() {
   Widget createWidgetUnderTest() {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (_) => mockCategoriesCubit),
         BlocProvider(create: (_) => mockMenuItemsCubit),
         BlocProvider(create: (_) => mockCartCubit),
       ],
       child: MaterialApp(
-        home: CategoriesPage(),
+        home: MenuItemsPage(
+          selectedCategory: mockCategoryHamburger,
+        ),
       ),
     );
   }
 
-  void arrangeCubitState({CategoriesState? categoriesState}) {
-    // Categories Cubit
-    categoriesState ??= CategoriesState.initial();
-    when(mockCategoriesCubit.getCategories())
-        .thenAnswer((_) async => mockCategoriesCubit);
-    when(mockCategoriesCubit.state).thenReturn(categoriesState);
+  void arrangeCubitState({MenuItemsState? menuItemsState}) {
+    // Menu Items Cubit for after navigating
+    menuItemsState ??= MenuItemsState.initial();
+    when(mockMenuItemsCubit.getMenuItemsById(mockCategoryHamburger.menuItems))
+        .thenAnswer((_) async => mockMenuItemsModelList);
+    when(mockMenuItemsCubit.state).thenReturn(menuItemsState);
 
     // Cart Cubit
     final cartState = CartState.initial();
     when(mockCartCubit.state).thenReturn(cartState);
-
-    // Menu Items Cubit for after navigating
-    final menuItemsState = MenuItemsState.initial();
-    when(mockMenuItemsCubit.getMenuItemsById(mockCategoryHamburger.menuItems))
-        .thenAnswer((_) async => mockCategoriesCubit);
-    when(mockMenuItemsCubit.state).thenReturn(menuItemsState);
   }
 
   testWidgets(
     'displays a CircularProgressIndicator when state is loading',
     (WidgetTester tester) async {
       arrangeCubitState(
-        categoriesState: CategoriesState(
+        menuItemsState: MenuItemsState(
           isLoading: true,
-          categories: [],
+          menuItems: [],
         ),
       );
 
@@ -87,9 +79,9 @@ void main() {
     'displays categories when state is loaded',
     (WidgetTester tester) async {
       arrangeCubitState(
-        categoriesState: CategoriesState(
+        menuItemsState: MenuItemsState(
           isLoading: false,
-          categories: mockCategoriesModelList,
+          menuItems: mockMenuItemsHamburgers,
         ),
       );
 
@@ -100,41 +92,14 @@ void main() {
       final itemFinder = find.byType(CircularProgressIndicator);
       expect(itemFinder, findsNothing);
 
-      // Should find a text that is behind scrolling
-      final itemFinder1 = find.text('The Krabby Patty Kollab');
+      // Should find a text that might be behind scrolling
+      final itemFinder1 = find.text('Dave\'s Single®');
       await tester.scrollUntilVisible(
         itemFinder1,
         500.0,
         scrollable: listFinder,
       );
       expect(itemFinder1, findsOneWidget);
-
-      // Reset scrolling for next search
-      await tester.drag(listFinder, Offset(0, double.maxFinite));
-      await tester.pumpAndSettle();
-
-      // Should find a text that is on screen
-      final itemFinder2 = find.text('Hamburgers');
-      await tester.scrollUntilVisible(
-        itemFinder2,
-        500.0,
-        scrollable: listFinder,
-        duration: Duration(seconds: 1),
-      );
-      expect(itemFinder2, findsOneWidget);
-
-      // Reset scrolling for next search
-      await tester.drag(listFinder, Offset(0, double.maxFinite));
-      await tester.pumpAndSettle();
-
-      // Should find a text that is behind scrolling
-      final itemFinder3 = find.text('The Krabby Patty Kollab');
-      await tester.scrollUntilVisible(
-        itemFinder3,
-        500.0,
-        scrollable: listFinder,
-      );
-      expect(itemFinder3, findsOneWidget);
     },
   );
 
@@ -142,16 +107,16 @@ void main() {
     'navigates to MenuItemsPage when category is tapped',
     (WidgetTester tester) async {
       arrangeCubitState(
-        categoriesState: CategoriesState(
+        menuItemsState: MenuItemsState(
           isLoading: false,
-          categories: mockCategoriesModelList,
+          menuItems: mockMenuItemsHamburgers,
         ),
       );
       await tester.pumpWidget(createWidgetUnderTest());
 
       // Should tap a text that might be behind scrolling
       final listFinder = find.byType(Scrollable);
-      final itemFinder = find.text('The Krabby Patty Kollab');
+      final itemFinder = find.text('Dave\'s Single®');
       await tester.scrollUntilVisible(
         itemFinder,
         500.0,
@@ -161,7 +126,7 @@ void main() {
       await tester.tap(itemFinder);
       await tester.pumpAndSettle();
 
-      expect(find.byType(MenuItemsPage), findsOneWidget);
+      expect(find.byType(SelectedMenuItemPage), findsOneWidget);
     },
   );
 }
